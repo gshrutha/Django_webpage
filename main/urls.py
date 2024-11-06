f705860f05b909d512864831e8d8ecb4f9d52251
G Shrutha Keerthi Reddy <kshrutha92@gmail.com>
2024-11-06 15:28:23 +0530

    modifications 1

--- main/urls.py
+++ main/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns =[
-path("<int:id>",views.index,name="index"),
 path("",views.home,name ="home"),
+path("<int:id>",views.index,name="index"),
 path("create/",views.create,name ="create")
 ]
